58b17e361fdfc783c2213957410f63b883a7a146
buffoon00 <todorovich.ute@gmail.com>
2022-07-30 13:22:03 +0300

    feat: solve reverse int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    
+    if (n > 0) {
+        const strN = String(n);
+        return strN.split('').reverse().join('');
+    } else {
+        m = -1 * n;
+        const strM = String(m);
+        return strM.split('').reverse().join('');
+    } 
 }
